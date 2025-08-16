498e02ab2c6897cb20a767d5572746349551bc97
Administrator <Administrator@EC2AMAZ-L311S9O>
2025-08-16 17:38:48 +0000

    Agregar archivos de proyecto.

---
+++ AplicacionNomina/Controllers/AccesoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionNomina.Controllers
+{
+    public class AccesoController : Controller
+    {
+
+        //GET: Acceso
+        public ActionResult Index()
+        {
+            return View();
+        }   
+
+        public ActionResult Autenticar()
+        {
+            return View();
+        }
+
+        //Post: Acceso
+        [HttpPost]
+        public ActionResult Autenticar(string usuario, string clave)
+        {
+            if (usuario == "admin" && clave == "1234")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Error = "Usuario o clave incorrectos.";
+                return View();
+            }
+        }
+    }
+}
