3e52fe405d563eaac59974b877f5fb60db2e9f4d
IngSudo <jerryvelez5@gmail.com>
2025-08-22 20:25:04 -0500

    Login connect

--- AplicacionNomina/Controllers/AccesoController.cs
+++ AplicacionNomina/Controllers/AccesoController.cs
@@ -4,6 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+//librerias conectar SQL
+using System.Data;
+using System.Data.SqlClient;
+
+using AplicacionNomina.Models;
+using System.Text;
+using System.Configuration;
+using System.Web.UI.WebControls;
+
+//encriptar
+using System.Security.Cryptography;
+
 namespace AplicacionNomina.Controllers
 {
     public class AccesoController : Controller
@@ -22,14 +34,22 @@ namespace AplicacionNomina.Controllers
 
         //Post: Acceso
         [HttpPost]
-        public ActionResult Autenticar(string usuario, string clave)
+        public ActionResult Autenticar(Employee oEmpleado)
         {
-            if (usuario == "admin" && clave == "1234")
+            try
             {
-                return RedirectToAction("Index", "Home");
+                using(SqlConnection cn=new SqlConnection(ConfigurationManager.ConnectionStrings["NominaContext"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("Select * from employees");
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    return RedirectToAction("Index", "Home");
+                }
             }
-            else
+            catch (Exception e)
             {
+                Console.WriteLine("Error" + e);
                 ViewBag.Error = "Usuario o clave incorrectos.";
                 return View();
             }
